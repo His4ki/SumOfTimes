b32d26cdb334cc3abaaf9499a6b53fecf82156e2
Kacper Kuźniak <kacperkuzniak@pc-2736.home>
2018-06-11 11:14:12 +0200

    Initial Commit

---
+++ SumOfTimes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SumOfTimes
+//
+//  Created by Kacper Kuźniak on 11/06/2018.
+//  Copyright © 2018 Kacper Kuźniak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
